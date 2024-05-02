3bb73c762f1ce34955c80070c432dc327794c3aa
lg.conrado <lg.conrado.lgc@gmail.com>
2024-05-02 17:34:29 -0300

    Estilização de Hover e Implementação de Logica do Filtro

--- src/components/Cardapio/index.tsx
+++ src/components/Cardapio/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import cardapio from "./cardapio.json";
 import styled from "styled-components";
 
@@ -6,10 +7,10 @@ const CustomSection = styled.section`
   flex-direction: column;
   gap: 64px;
   width: 100%;
-  `;
+`;
 
 const CustomButtonsContainer = styled.div`
-justify-content: center;
+  justify-content: center;
   display: flex;
   width: 100%;
 `;
@@ -25,6 +26,7 @@ const CustomButtons = styled.div`
 
 const CustomButton = styled.button`
   appearance: none;
+  border-radius: 64px;
   height: 48px;
   cursor: pointer;
   border: none;
@@ -32,6 +34,17 @@ const CustomButton = styled.button`
   color: var(--cor-secundaria);
   width: 200px;
   font-size: 1.25rem;
+  transition: 0.2s background-color ease-in-out, 0.2s color ease-in-out;
+
+  &:hover {
+    color: var(--cor-fundo);
+    background-color: var(--cor-primaria);
+  }
+
+  &[aria-actived="true"] {
+    color: var(--cor-fundo);
+    background-color: var(--cor-primaria);
+  }
 `;
 
 const CustomCardapio = styled.div`
@@ -52,7 +65,8 @@ const CustomSectionTitle = styled.h2`
 const CustomOptions = styled.div`
   display: grid;
   flex-direction: column;
-  gap: 16px;
+  column-gap: 32px;
+  row-gap: 16px;
   padding: 0 32px;
   grid-template-columns: repeat(2, 1fr);
 `;
@@ -60,47 +74,78 @@ const CustomOptions = styled.div`
 const CustomOption = styled.span`
   display: flex;
   justify-content: space-between;
+  padding: 4px 0;
+  transition: 0.2s background-color ease-in-out;
+
+  &:hover {
+    background-color: #181818;
+
+    > h3 {
+      color: var(--cor-secundaria);
+    }
+  }
 `;
 
-const CustomDescriptionOption = styled.h3``;
+const CustomDescriptionOption = styled.h3`
+  transition: 0.2s color ease-in-out;
+`;
 
 const Cardapio = () => {
+  const [filtro, setFiltro] = useState("");
   return (
     <CustomSection>
       <CustomButtonsContainer>
         <CustomButtons>
           {Object.entries(cardapio[0]).map((modulo: any, index) => {
-            console.log(modulo[0]);
-            return <CustomButton>{modulo[0]}</CustomButton>;
+            return (
+              <CustomButton
+                key={index}
+                onClick={() => {
+                  console.log(modulo[0]);
+                  if (filtro === modulo[0]) {
+                    setFiltro("");
+                  } else {
+                    setFiltro(modulo[0]);
+                  }
+                }}
+                aria-actived={filtro === modulo[0]}
+              >
+                {modulo[0]}
+              </CustomButton>
+            );
           })}
         </CustomButtons>
       </CustomButtonsContainer>
-      {Object.entries(cardapio[0]).map((modulo: any, index) => {
-        return (
-          <CustomCardapio key={index}>
-            <CustomSectionTitleContainer>
-              <CustomSectionTitle>{modulo[0]}</CustomSectionTitle>
-            </CustomSectionTitleContainer>
-            <CustomOptions>
-              {modulo[1].map((opcao: any, index: number) => {
-                return (
-                  <CustomOption key={index}>
-                    <CustomDescriptionOption>
-                      {opcao.titulo}
-                    </CustomDescriptionOption>
-                    <CustomDescriptionOption>
-                      {opcao.valor.toLocaleString(`pt-br`, {
-                        style: "currency",
-                        currency: "BRL",
-                      })}
-                    </CustomDescriptionOption>
-                  </CustomOption>
-                );
-              })}
-            </CustomOptions>
-          </CustomCardapio>
-        );
-      })}
+      {Object.entries(cardapio[0])
+        .filter((item: any) => {
+          return filtro === "" ? true : item[0] === filtro ? true : false;
+        })
+        .map((modulo: any, index) => {
+          return (
+            <CustomCardapio key={index}>
+              <CustomSectionTitleContainer>
+                <CustomSectionTitle>{modulo[0]}</CustomSectionTitle>
+              </CustomSectionTitleContainer>
+              <CustomOptions>
+                {modulo[1].map((opcao: any, index: number) => {
+                  return (
+                    <CustomOption key={index}>
+                      <CustomDescriptionOption>
+                        {opcao.titulo}
+                      </CustomDescriptionOption>
+                      <CustomDescriptionOption>
+                        {opcao.valor.toLocaleString(`pt-br`, {
+                          style: "currency",
+                          currency: "BRL",
+                        })}
+                      </CustomDescriptionOption>
+                    </CustomOption>
+                  );
+                })}
+              </CustomOptions>
+            </CustomCardapio>
+          );
+        })}
     </CustomSection>
   );
 };
